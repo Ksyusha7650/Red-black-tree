1d00f638920d686c97265852fd39320529897949
Ksyusha <43499411+Ksyusha7650@users.noreply.github.com>
2022-02-27 18:37:31 +0300

    Update Program.cs

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -11,11 +11,10 @@ class Program
         int amount = 0;
         List<Int32> elements = new List<Int32>();
         Interface.greetings();
-        Interface.input_amount(ref amount);
-        Interface.choose_input(amount, ref elements);
+        Interface.choose_input(ref elements);
         Tree tree = new Tree();
-        Tree.make_tree(elements, ref tree);
-        tree.display_tree(tree);
+        Tree.MakeTree(elements, ref tree);
+        tree.DisplayTree(tree);
         Interface.menu_tree(tree, ref elements);
     }
 }
