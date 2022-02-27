552984173144ab6e0b7a1fae6dd59daa1c9f828e
Ksyusha <43499411+Ksyusha7650@users.noreply.github.com>
2022-02-27 18:33:04 +0300

    Update Interface.cs

--- Task 1/Interface.cs
+++ Task 1/Interface.cs
@@ -9,7 +9,7 @@ namespace Task_1
 {
     public enum Menu { ADD = 1, DELETE, SEARCH, ORDER, HEIGHT, SHOW, MIN, MAX, SAVE, EXIT };
 
-    public enum Type_input { MANUALLY = 1, RANDOMLY, FILE }
+    public enum typeInput { MANUALLY = 1, RANDOMLY, FILE }
     public class Interface
     {
         public static void greetings()
@@ -24,46 +24,49 @@ namespace Task_1
 
         public static void input_amount(ref int amount)
         {
-            bool check_input = false;
+            bool checkInput = false;
             Console.WriteLine("Введите количество элементов: ");
-            while (!check_input)
+            while (!checkInput)
             {
-                amount = get_int(ref check_input);
+                amount = GetInt(ref checkInput);
                 if (amount <= 0)
                 {
                     Console.WriteLine("Количество элементов не может быть неположительным, повторите ввод: ");
-                    check_input = false;
+                    checkInput = false;
                 }
             }
         }
-        public static void choose_input(int amount, ref List<Int32> elements)
+        public static void choose_input(ref List<Int32> elements)
         {
-            bool check_input = false;
+            bool checkInput = false;
             int choice = 0;
+            int amount = 0;
             Console.WriteLine("Выберите способ заполнения дерева: "
             + Environment.NewLine + "[1] - Ввод вручную" + Environment.NewLine + "[2] - Ввод случайным образом"
             + Environment.NewLine + "[3] - Взять данные из файла");
-            Type_input type_input = Type_input.MANUALLY;
-            while (!check_input)
+            typeInput type_input = typeInput.MANUALLY;
+            while (!checkInput)
             {
-                choice = get_int(ref check_input);
-                type_input = (Type_input)choice;
-                if ((type_input < Type_input.MANUALLY) || (type_input > Type_input.FILE))
+                choice = GetInt(ref checkInput);
+                type_input = (typeInput)choice;
+                if ((type_input < typeInput.MANUALLY) || (type_input > typeInput.FILE))
                 {
                     Console.WriteLine("Введите либо 1, либо 2, либо 3.");
-                    check_input = false;
+                    checkInput = false;
                 }
             }
             switch (type_input)
             {
-                case Type_input.MANUALLY:
-                    input_manually(amount, ref elements);
+                case typeInput.MANUALLY:
+                    Interface.input_amount(ref amount);
+                    InputManually(amount, ref elements);
                     break;
-                case Type_input.RANDOMLY:
-                    input_randomally(amount, ref elements);
+                case typeInput.RANDOMLY:
+                    Interface.input_amount(ref amount);
+                    InputRandomally(amount, ref elements);
                     break;
-                case Type_input.FILE:
-                    file_input(amount, ref elements);
+                case typeInput.FILE:
+                    FileInput(ref elements);
                     break;
             }
             Console.WriteLine("Введенные элементы: ");
@@ -73,7 +76,7 @@ namespace Task_1
 
         public static void menu_tree(Tree tree, ref List<Int32> elements)
         {
-            bool check_input = false;
+            bool checkInput = false;
             int choice = 0;
             int key = 0;
             Menu menu_choice = Menu.ADD;
@@ -84,84 +87,84 @@ namespace Task_1
             + Environment.NewLine + "[5] - Высота дерева" + Environment.NewLine + "[6] - Показать дерево"
             + Environment.NewLine + "[7] - Минимальный элемент дерева" + Environment.NewLine + "[8] - Максимальный элемент дерева"
             + Environment.NewLine + "[9] - Сохранить дерево в файл" + Environment.NewLine + "[10] - Выйти");
-            while (!check_input)
+            while (!checkInput)
             {
-                choice = get_int(ref check_input);
+                choice = GetInt(ref checkInput);
                 menu_choice = (Menu)choice;
                 if ((menu_choice < Menu.ADD) || (menu_choice > Menu.EXIT))
                 {
                     Console.WriteLine("Такого пункта в меню нет. Повторите ввод: ");
-                    check_input = false;
+                    checkInput = false;
                 }
             }
             switch (menu_choice)
             {
                 case Menu.ADD:
-                    check_input = false;
+                    checkInput = false;
                     Console.WriteLine("Введите значение элемента, который хотите вставить: ");
-                    while (!check_input)
+                    while (!checkInput)
                     {
-                        key = get_int(ref check_input);
+                        key = GetInt(ref checkInput);
                     }
-                    tree.insert_node(ref tree.root, key);
+                    tree.InsertNode(ref tree.root, key);
                     break;
                 case Menu.DELETE:
-                    check_input = false;
+                    checkInput = false;
                     Console.WriteLine("Введите значение элемента, который хотите удалить: ");
-                    while (!check_input)
+                    while (!checkInput)
                     {
-                        key = get_int(ref check_input);
+                        key = GetInt(ref checkInput);
                     }
-                    tree.delete_node(tree, key);
+                    tree.DeleteNode(tree, key);
                     menu_tree(tree, ref elements);
                     break;
                 case Menu.SEARCH:
-                    check_input = false;
+                    checkInput = false;
                     Console.WriteLine("Введите значение элемента, которого хотите найти: ");
-                    while (!check_input)
+                    while (!checkInput)
                     {
-                        key = get_int(ref check_input);
+                        key = GetInt(ref checkInput);
                     }
-                    TreeNode searched = tree.tree_search(key, tree.root);
+                    TreeNode searched = tree.TreeSearch(key, tree.root);
                     if (searched != null)
                         Console.WriteLine(searched.data);
                     break;
                 case Menu.ORDER:
                     Console.WriteLine("Прямой обход дерева: ");
-                    tree.preorder_tree_walk(tree.root);
+                    tree.PreorderTreeWalk(tree.root);
                     Console.WriteLine();
                     Console.WriteLine("Центрированный обход дерева: ");
-                    tree.inorder_tree_walk(tree.root);
+                    tree.InorderTreeWalk(tree.root);
                     Console.WriteLine();
                     Console.WriteLine("Обратный обход дерева: ");
-                    tree.postorder_tree_walk(tree.root);
+                    tree.PostorderTreeWalk(tree.root);
                     break;
 
                 case Menu.HEIGHT:
-                    Console.WriteLine("Высота дерева = " + tree.height(tree).ToString());
+                    Console.WriteLine("Высота дерева = " + tree.Height(tree).ToString());
                     break;
                 case Menu.SHOW:
-                    tree.display_tree(tree);
+                    tree.DisplayTree(tree);
                     break;
                 case Menu.MIN:
                     if (tree.root != null)
-                        Console.WriteLine("Минимальный элемент = " + tree.tree_minimum(tree.root).data);
+                        Console.WriteLine("Минимальный элемент = " + tree.TreeMinimum(tree.root).data);
                     else Console.WriteLine("Минимального элемента нет");
                     break;
                 case Menu.MAX:
                     if (tree.root != null)
-                        Console.WriteLine("Максимальный элемент = " + tree.tree_maximum(tree.root).data);
+                        Console.WriteLine("Максимальный элемент = " + tree.TreeMaximum(tree.root).data);
                     else Console.WriteLine("Минимального элемента нет");
                     break;
                 case Menu.SAVE:
-                    Algorithms.file_save(ref elements);
+                    Algorithms.FileSave(ref elements);
                     break;
                 case Menu.EXIT:
                     Console.WriteLine("Вы уверены?" + Environment.NewLine + "[1] - Да" + Environment.NewLine + "[2] - Нет");
-                    check_input = false;
-                    while (!check_input)
+                    checkInput = false;
+                    while (!checkInput)
                     {
-                        choice = get_int(ref check_input);
+                        choice = GetInt(ref checkInput);
                     }
                     switch (choice)
                     {
